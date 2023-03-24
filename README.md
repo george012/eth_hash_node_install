169b7b4fc99e53aa6fe54f41c022c93318130dfe
WMYeah_Mac <george012@163.com>
2023-03-24 23:46:47 +0800

    66

--- README.md
+++ README.md
@@ -1,5 +1,17 @@
-# one_key_install_core-geth
-One-click installation of core-geth
-# core-geth Infos
+<font size=20>目录：</font>
+<!-- TOC -->
+
+- [1. `summary`(简介)](#1-summary简介)
+- [2. `core-geth Infos` (core-geth 相关信息)](#2-core-geth-infos-core-geth-相关信息)
+
+<!-- /TOC -->
+
+# 1. `summary`(简介)
+* This is a script to install the latest version of the Ethereum Classic node with one click
+* `zh-CN--->`这是一个一键安装以太经典节点最新版本的脚本
+
+# 2. `core-geth Infos` (core-geth 相关信息)
 * [core-geth github](https://github.com/etclabscore/core-geth)
 * [core-geth Documentation](https://etclabscore.github.io/core-geth/)
+
+
--- one_key_install_etc_node.sh
+++ one_key_install_etc_node.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
-
+produckName="One Key Install ETC Node"
 
 parse_json(){
     echo "${1//\"/}" | tr -d '\n' | tr -d '\r' | sed "s/.*$2:\([^,}]*\).*/\1/"
 }
 
-
+# zh-CN---:创建一个新的systemd服务文件
+# en-US---:Create a new systemd service file
 create_geth_service(){
 rm -rf /etc/systemd/system/core-geth.service
 systemctl daemon-reload
-# 创建一个新的systemd服务文件
 cat << EOF | sudo tee /etc/systemd/system/core-geth.service
 [Unit]
 Description=Core-Geth Ethereum node
@@ -25,24 +25,26 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-# 开机自启Core-Geth服务
 sudo systemctl enable core-geth
 
-# 显示Core-Geth服务状态
 sudo systemctl status core-geth
 }
 
+# zh-CN---:优化系统配置以支持更高的并发连接
+# en-US---:Optimize system configuration to support higher concurrent connections
 optimize_network(){
-    # 优化系统配置以支持更高的并发连接,利用已存在脚本优化
+    
     wget --no-check-certificate https://raw.githubusercontent.com/george012/gt_script/master/optimize_network.sh && chmod a+x ./optimize_network.sh && ./optimize_network.sh
 }
 
 download_latest_geth(){
-    # 获取最新版本的信息
+    # zh-CN---:获取最新版本的信息
+    # en-US---:Get the latest version of the information
     LATEST_RELEASE_INFO=$(curl --silent https://api.github.com/repos/etclabscore/core-geth/releases/latest)
     GETH_VERSION=$(parse_json "$LATEST_RELEASE_INFO" "tag_name")
 
-    # 从JSON响应中提取下载链接（请根据您的Linux发行版和架构修改）
+    # zh-CN---:筛选linux版本
+    # en-US---:Filter linux version
     DOWNLOAD_LINK=$(echo "$LATEST_RELEASE_INFO" | grep -oP '"browser_download_url": "\K(.*linux.*)(?=")')
 
 
@@ -83,40 +85,30 @@ pre_config(){
 pre_config && wait && download_latest_geth && wait && create_geth_service && wait && optimize_network
 
 echo "============================ ${produckName} ============================"
-echo "  1、"
-echo "  2、更新 ${produckName}"
-
+echo "  1、Install core-geth、Create Systemctl Serveice、Optimize Network(安装core-geth、创建Systemctl服务、优化网络)"
+echo "  2、Install core-geth And Create Systemctl Serveice(安装 core-geth 并创建 Systemctl 服务)"
+echo "  3、Just Only Download core-geth(只下载 core-geth)"
+echo "  4、Just Only Create core-geth Systemctl Serveice(只创建 core-geth Systemctl 服务)"
+echo "  5、Just Only Optimize Network(只优化网络)"
 echo "======================================================================"
-read -p "$(echo -e "请选择[1-6]：")" choose
+read -p "$(echo -e "Plase Choose [1-5]：(请选择[1-5]：)")" choose
 case $choose in
 1)
-    install
+    pre_config && wait && download_latest_geth && wait && create_geth_service && wait && optimize_network
     ;;
 2)
-    update
+    pre_config && wait && download_latest_geth && wait && create_geth_service
     ;;
 3)
-    uninstall
+    pre_config && wait && download_latest_geth
     ;;
 4)
-    start
+    create_geth_service
     ;;
 5)
-    restart
-    ;;
-6)
-    stop
-    ;;
-7)
-    show_log
-    ;;
-8)
-    check_limit
-    ;;
-9)
-    uninstall_tx_mon
+    optimize_network
     ;;
 *)
-    echo "输入错误，请重新输入！"
+    echo "Input Error，Plase Again(输入错误，请重试)"
     ;;
 esac
