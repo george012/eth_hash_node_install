adef5daa3fd92874f6f7a5b56a27c6b112719566
TaoTao <george012@163.com>
2023-03-25 04:24:00 +0800

    修正

--- one_key_install_etc_node.sh
+++ one_key_install_etc_node.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 produckName="One Key Install ETC Node"
+CORE_GETH_Dir=/core-geth
 
 parse_json(){
     echo "${1//\"/}" | tr -d '\n' | tr -d '\r' | sed "s/.*$2:\([^,}]*\).*/\1/"
@@ -8,8 +9,8 @@ parse_json(){
 # zh-CN---:创建一个新的systemd服务文件
 # en-US---:Create a new systemd service file
 create_geth_service(){
-rm -rf /etc/systemd/system/core-geth.service
-systemctl daemon-reload
+sudo rm -rf /etc/systemd/system/core-geth.service
+sudo systemctl daemon-reload
 cat << EOF | sudo tee /etc/systemd/system/core-geth.service
 [Unit]
 Description=Core-Geth Ethereum node
@@ -17,7 +18,7 @@ After=network.target
 
 [Service]
 User=root
-ExecStart=/geth/geth --http --http.addr "0.0.0.0" --http.port "8545" --http.api "eth,web3,net" --cache 1024 --ethash.dagdir "/geth/.ethash" --datadir "/geth/.ethereum" --chain "classic" --syncmode "full" --gcmode "archive"
+ExecStart=$CORE_GETH_Dir/geth --http --http.addr "0.0.0.0" --http.port "8545" --http.api "eth,web3,net" --cache 1024 --ethash.dagdir "$CORE_GETH_Dir/.ethash" --datadir "$CORE_GETH_Dir/.ethereum" --chain "classic" --syncmode "full" --gcmode "archive"
 Restart=always
 RestartSec=3
 
@@ -25,9 +26,10 @@ RestartSec=3
 WantedBy=multi-user.target
 EOF
 
-sudo systemctl enable core-geth
-
-sudo systemctl status core-geth
+ln -s /etc/systemd/system/core-geth.service /etc/systemd/system/geth.service \
+&& sudo systemctl daemon-reload \
+&& sudo systemctl enable geth \
+&& sudo systemctl status geth
 }
 
 # zh-CN---:优化系统配置以支持更高的并发连接
@@ -40,42 +42,50 @@ optimize_network(){
 download_latest_geth(){
     # zh-CN---:获取最新版本的信息
     # en-US---:Get the latest version of the information
-    LATEST_RELEASE_INFO=$(curl --silent https://api.github.com/repos/etclabscore/core-geth/releases/latest)
-    GETH_VERSION=$(parse_json "$LATEST_RELEASE_INFO" "tag_name")
+    local LATEST_RELEASE_INFO=$(curl --silent https://api.github.com/repos/etclabscore/core-geth/releases/latest)
+    local GETH_VERSION=$(parse_json "$LATEST_RELEASE_INFO" "tag_name")
 
     # zh-CN---:筛选linux版本
     # en-US---:Filter linux version
-    DOWNLOAD_LINK=$(echo "$LATEST_RELEASE_INFO" | grep -oP '"browser_download_url": "\K(.*linux.*)(?=")')
+    local DOWNLOAD_LINK=$(echo "$LATEST_RELEASE_INFO" | grep -oP '"browser_download_url": "\K(.*linux.*)(?=")')
 
 
-    DOWNLOAD_LINK_ARRAY=(${DOWNLOAD_LINK// / })
+    local DOWNLOAD_LINK_ARRAY=(${DOWNLOAD_LINK// / })
 
-    for i in "${!DOWNLOAD_LINK_ARRAY[@]}"; do
-        aurl=${DOWNLOAD_LINK_ARRAY[i]}
-        file_name=`echo ${aurl##*'/'}`
-        wget wget --no-check-certificate $aurl \
-        && wait \
-        && echo "下载："$aurl"完成",文件为：$file_name
+    local CORE_GETH_DOWNLOAD_URL=""
+    for aurl in "${DOWNLOAD_LINK_ARRAY[@]}"; do
+        if [[ ! $aurl =~ \.sha256d$ ]] && [[ ! $aurl =~ alltools ]]; then
+            $CORE_GETH_DOWNLOAD_URL=aurl
+        fi
     done
 
+    file_name=`echo ${CORE_GETH_DOWNLOAD_URL##*'/'}`
+    wget wget --no-check-certificate $CORE_GETH_DOWNLOAD_URL \
+    && wait \
+    && echo "下载："$CORE_GETH_DOWNLOAD_URL"完成",文件为：$file_name \
 
-    # # 解压缩下载的文件
-    # tar -xzf core-geth.tar.gz
 
-    # # 获取解压后的目录名
-    # DIRECTORY_NAME=$(echo "$DOWNLOAD_LINK" | grep -oP "core-geth-.*-1")
+    # 检查目标目录是否存在，如果不存在则创建目录
+    if [ ! -d "$CORE_GETH_Dir" ]; then
+        mkdir -p "$CORE_GETH_Dir"
+    fi
 
-    # # 创建/geth目录（如果不存在）
-    # mkdir -p /geth
+    # 检查geth文件是否存在，如果存在则备份
+    if [ -f "$CORE_GETH_Dir/geth" ]; then
+        old_version=`$CORE_GETH_Dir/geth version`
+        echo Old Version With:$old_version
+        current_datetime=$(date +"%Y%m%d%H%M%S")
+        mv "$CORE_GETH_Dir/geth" "$CORE_GETH_Dir/geth.$current_datetime.bak"
+    fi
 
-    # # 将geth二进制文件移动到/geth目录
-    # mv $DIRECTORY_NAME/geth /geth/
+    # 解压缩文件到指定目录
+    unzip "$file_name" -d "$CORE_GETH_Dir"
 
-    # # 删除下载的压缩包和解压后的目录
-    # rm -rf core-geth.tar.gz $DIRECTORY_NAME
+    # 删除下载的压缩包和解压后的目录
+    rm -rf "$file_name"
 
-    # # 输出geth版本信息以验证安装成功
-    # /geth/geth version
+    # 输出geth版本信息以验证安装成功
+    $CORE_GETH_Dir/geth version
 }
 
 pre_config(){
